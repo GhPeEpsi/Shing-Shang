f0645b40c60ff1be27a6f5c2bfa949ca2245285b
GhPeEpsi <bastien.p1@hotmail.com>
2017-12-21 23:23:07 +0100

    6é test Bastien en étant cette fois dans le bon dossier

---
+++ Projet_1A_Shing_Shang/Fichiers_sources/main.c
@@ -0,0 +1,32 @@
+#include "header.h"
+
+void main()
+{
+
+ //###### coment #######
+ //Cette fonction est le début ce sera celle qui sera lancé
+ //
+
+ //salut je fais un test (Bastien)
+
+ //creation des variables, pseudo, plateau, ... :
+ perso pseudo1, pseudo2;
+ int tmp;
+ case_pla plateau[HAU_PLA][LAR_PLA];
+ deplacement move;
+ printf("salut");
+
+ //on commence par afficher l'entéte :
+ //entete();
+
+ remp_pla(plateau);
+ aff_pla(plateau);
+
+ //lancement des demandes de jeu :
+ demande(&move);
+
+ //lancement des verifs
+ res=choix_verif(move, plateau);
+ printf("resultat : %d",res);
+ //choix_singe(move, plateau);
+}
